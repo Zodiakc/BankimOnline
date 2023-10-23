8a0436a38860d16854b269990b71f3fd935b427d
Zodiakc <mbinternet@mail.ru>
2023-10-23 18:51:36 +0300

    fix text

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
         <>
             <main className="bg-dark min-h-screen">
                 <div className="container mx-auto px-2 ">
-                    <h1 className="sm:text-5xl pt-32 text-[31px] text-center">
+                    <h1 className="sm:text-5xl sm:text-left pt-32 text-[31px] text-center">
                         Рассчитайте ипотеку быстро и просто
                     </h1>
 
